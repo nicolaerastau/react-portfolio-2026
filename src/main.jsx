7ff20f22db0eecf9bc731a7ed1ac36d1c38a7c76
Nicolae Rastau <nickqsr@yahoo.com>
2026-05-17 08:52:40 +0200

    change to createHashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from '@/App.jsx';
 import Home from '@/sections/Home';
@@ -8,7 +8,7 @@ import About from '@/sections/About';
 import Projects from '@/sections/Projects';
 import Contact from '@/sections/Contact';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: '/',
@@ -34,7 +34,7 @@ const router = createBrowserRouter(
     },
   ],
   {
-    basename: '/react-portfolio-2026',
+    basename: '/',
   },
 );
 
